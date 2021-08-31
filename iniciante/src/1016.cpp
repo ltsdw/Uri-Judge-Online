ec7de70852fc8322f7222f46ac8877cc4c295070
ltsdw <44977415+ltsdw@users.noreply.github.com>
2021-08-31 14:35:22 -0300

    1016 - Distância

---
+++ iniciante/src/1016.cpp
@@ -0,0 +1,19 @@
+/* title: Distância
+ * number of the problem: 1016
+ * date: 31/08/2021
+ * author: ltsdw
+ */
+
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+    using std::cin, std::cout;
+
+    int x;
+    cin >> x;
+
+    cout << x * 2 << " minutos" << '\n';
+
+    return EXIT_SUCCESS;
+}
